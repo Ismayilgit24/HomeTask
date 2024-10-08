28c86ef762a085aaabe30ce11429debe0413f41c
Ismayilgit24 <ismayilibrahinzad9@gmail.com>
2024-10-08 11:36:50 +0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿//Task-1
+
+//int number = 66;
+//int maxDigit = 0;
+
+//while (number > 0)
+//{
+//	int digit1 = number % 10;
+//	if (digit1 > maxDigit)
+//	{
+//		maxDigit = digit1;
+//	}
+//	number /= 10;
+//}
+//Console.WriteLine(maxDigit);
+
+
+
+
+//Task-2
+
+//int number = 2;
+
+//if (number % 2 == 0 && number == 2)
+//{
+//	Console.WriteLine("Ədəd sadədir");
+//}
+//else
+//{
+//	Console.WriteLine("Ədəd mürəkkəbdir");
+//}
+
+
+
+//Task-2  
+
+//Console.WriteLine("Ededi daxil edin :");
+//int num = Convert.ToInt32(Console.ReadLine());
+// // 1 2 3 4 5 6 7 8 9 10 11 12 13
+//for (int i=2; i<num; i++)
+//{
+//	if(num%i==0)
+//	{
+
+//        Console.WriteLine("murekkeb");
+
+//		break;
+//	}
+//	else
+//	{
+//        Console.WriteLine("sade");
+//		break;
+//	}
+//}
+
+
+
+
+
+//Task-3
+
+//int number = 8;
+//int powerofTwo = 1;
+
+//while (powerofTwo < number)
+//{
+//    powerofTwo *= 2;
+//}
+//if (powerofTwo == number && powerofTwo != 1)
+//{
+//    Console.WriteLine("Qüvvətidir");
+//}
+//else
+//{
+//    Console.WriteLine("Qüvvəti deyil");
+//}
+
+
+ 
+
